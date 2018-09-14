c451920d56924bfedc8a895e1909fe9b1463dbb0
DamBai212 <damanybailey@icloud.com>
2018-09-14 16:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,11 @@ return Math.abs((feet - distance) * 264);
 }
 
 function calculatesFarePrice(start, destination){
-let num = distanceTravelledInFeet(start, destination) - 400;
-     if(num < 0){
-       return 0;
-     }
+let num = distanceTravelledInFeet(start, destination);
+     if(num < 400){
+       return 0
+     } else if (num > 400 && num < 2000){
+       return 2.56
+     } else if (num > 2000 && num < 2500)
+     return 25
 }
