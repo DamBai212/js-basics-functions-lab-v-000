bf7aece4942a7b9990f4baf594e2bcc3058353f0
DamBai212 <damanybailey@icloud.com>
2018-09-14 15:52:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+ return  Math.abs(distance - 42);
+}
+
+
+function distanceFromHqInFeet(feet){
+return distanceFromHqInBlocks(feet) * 264
+}
+
+
+function distanceTravelledInFeet(distance, feet){
+return Math.abs((feet - distance) * 264);
+}
+
+function calculatesFarePrice(start, destination){
+  return "hi"
+}
